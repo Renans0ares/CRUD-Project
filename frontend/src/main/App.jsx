0b687ca97ffc425d107f3090df98784cf8c0ff58
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-03-05 23:33:12 -0300

    Update App.jsx

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,15 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter} from 'react-router-dom'
 
-import Logo from '../components/template/Header'
+import Routes from './Routes'
+import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Main />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
+    
