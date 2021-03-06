8eae625f4206bdb116fc65673309aa4104cecdb3
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-03-05 23:34:43 -0300

    Update Footer.jsx

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+            <strong> Renan</strong>
+        </span>
     </footer>
