73e74d67f02d29bd216dfadeca35d11ce226da27
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-03-05 23:35:47 -0300

    Update Logo.jsx

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,7 +1,12 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default props => 
     <aside className="logo">
-        Logo
+        <Link to="/" className="logo">
+            <img src={logo} alt="logo" />
+        </Link>
     </aside>
